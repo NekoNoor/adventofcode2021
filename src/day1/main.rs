66c3c0036ec2ff86af6064c7474607e784ff5dd2
Norah Hannah Schat <nschat@student.codam.nl>
2021-12-02 14:19:49 +0100

    feat: implement day1 in rust

---
+++ src/day1/main.rs
@@ -0,0 +1,19 @@
+use std::io::*;
+use std::fs::File;
+
+fn count_increment(input: &Vec<i32>) -> usize {
+    return input.windows(2).filter(|t| t[0] < t[1]).count();
+}
+
+fn main() {
+    let file = File::open("./input/day1");
+    let reader = BufReader::new(file.unwrap());
+
+    let vector: Vec<i32> = reader.lines().map(|x| x.unwrap().parse().unwrap()).collect();
+
+    println!("part1: {}", count_increment(&vector));
+
+    let sum_vector: Vec<i32> = vector.windows(3).map(|x| x.iter().sum::<i32>()).collect();
+
+    println!("part2: {}", count_increment(&sum_vector));
+}
